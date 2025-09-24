92c0dff4059763e926b9ae3bb365364878053e64
akopan <akopan@godaddy.com>
2025-09-24 13:15:46 -0700

    fix publish location

--- FloconAndroid/publish.gradle.kts
+++ FloconAndroid/publish.gradle.kts
@@ -1,4 +1,4 @@
 // Root Android project publish configuration
 // This script applies the centralized publishing configuration from Flocon-dist
 
-apply(from = "../../dist/publish.gradle")
+apply(from = "../../../FloconDistribution/publish.gradle")
